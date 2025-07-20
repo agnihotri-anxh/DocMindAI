6630cacbde898d7ab8d5a49658b5bcbd92239294
Ansh Agnihotri <agnihotriansh05@gmail.com>
2025-07-20 23:06:50 +0530

    Changed the port main.py

--- backend/main.py
+++ backend/main.py
@@ -230,4 +230,4 @@ def test():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="127.0.0.1", port=port, log_level="info") 
+    uvicorn.run(app, host="0.0.0.0", port=port, log_level="info")
